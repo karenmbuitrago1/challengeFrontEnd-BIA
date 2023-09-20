0d34d2f74a10082433ba43b267b38af4adead216
karenmbuitrago1 <karenmbuitrago@gmail.com>
2023-09-20 16:43:08 -0500

    add new search filter by country

---
+++ src/components/CountrySearch.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+const CountrySearch = ({ onSearch }) => {
+	const [searchCountry, setSearchCountry] = useState("");
+
+	const handleSearch = () => {
+		onSearch(searchCountry)
+	}
+
+	const handleChange = (e) => {
+		const { value } = e.target;
+		setSearchCountry(value)
+		onSearch(value);
+	}
+
+	return (
+		<div className="flags__search">
+			<form className="flags__search--container">
+				<span className="material-symbols-outlined --search">search</span>
+				<input
+					className="flags__search-input"
+					type="search"
+					placeholder="Search for a country..."
+					value={searchCountry}
+					onChange={handleChange}
+				/>
+			</form>
+		</div>
+	)
+}
+
+export default CountrySearch;
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,40 +1,66 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios"; // Agrega esta línea de importación
+import axios from "axios";
+import CountrySearch from "./CountrySearch";
 
 const FlagsDashboard = () => {
   const [countries, setCountries] = useState([]);
+  const [searchCountry, setSearchCountry] = useState("");
+  const [filteredCountries, setFilteredCountries] = useState([]);
 
   useEffect(() => {
-    axios.get("/api/countries") // Ruta a la API creada en pages/api
+    axios
+      .get("/api/countries")
       .then((response) => {
         setCountries(response.data);
+        setFilteredCountries(response.data);
       })
       .catch((error) => {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       });
   }, []);
 
-  console.log(countries, "hola")
+  // Función para manejar la búsqueda
+  const handleSearch = (searchTerm) => {
+    const filtered = countries.filter((country) =>
+      country.name.common.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setFilteredCountries(filtered);
+    setSearchCountry(searchCountry); // Actualiza el término de búsqueda en el estado
+  };
 
   return (
-    <ul className="list__countries">
-      {countries.map((country, index) => (
-        <li className="country" key={index}>
-          <div className="article-wrapper">
-            <figure>
-              <img src={country.flags.png} alt="" />
-            </figure>
-            <div className="article-body">
-              <h2>{country.name.common}</h2>
-              <p> Population:{country.population} </p>
-              <p> Region:{country.region} </p>
-              <p> Capital:{country.capital} </p>
+    <div className="flags">
+      <CountrySearch onSearch={handleSearch} />
+      <ul className="flags__dashboard">
+        {filteredCountries.map((country, index) => (
+          <li className="flags__country" key={index}>
+            <div className="country__article">
+              <figure className="country__article-figure">
+                <img
+                  className="country__article-img"
+                  src={country.flags.png}
+                  alt=""
+                />
+              </figure>
+              <div className="country__article__body article__body">
+                <h2 className="article__heading">{country.name.common}</h2>
+                <div className="article__text">
+                  <p className="article__data-item">
+                    <strong>Population:</strong> {country.population}
+                  </p>
+                  <p className="article__data-item">
+                    <strong>Region:</strong> {country.region}
+                  </p>
+                  <p className="article__data-item">
+                    <strong>Capital:</strong> {country.capital}
+                  </p>
+                </div>
+              </div>
             </div>
-          </div>
-        </li>
-      )
-      )}
-    </ul>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
